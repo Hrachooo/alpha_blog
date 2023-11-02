eea70767f5d9cd2b2a73a0f7fac8b2f2f8df01bc
Hrachya Movsisyan <hm@bic.com.de>
2023-11-02 13:27:19 +0100

    added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
-    
+    validates :username, presence: true,
+                length: {minimum: 3, maximmum: 25},
+                uniqueness: {case_sensitive: false}
+    EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true,
+                length: {minimum: 10, maximum: 105},
+                uniqueness: {case_sensitive: false},
+                format: { with: EMAIL_REGEX }
 end 
